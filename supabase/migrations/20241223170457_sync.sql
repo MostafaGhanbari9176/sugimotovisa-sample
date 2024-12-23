aa23037cdd68be7e3a235d499fe433b4c7cd0f05
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-12-23 23:15:06 +0330

    fix: fixing tables columns name

--- supabase/migrations/20241223170457_sync.sql
+++ supabase/migrations/20241223170457_sync.sql
@@ -2,7 +2,7 @@
 -- this create author table if not exists
 -- another table have reference to this table so we should create this first
 create table if not exists author (
-    id uuid primary key default gen_random_uuid() not null,
+    author_id uuid primary key default gen_random_uuid() not null,
     name varchar(30) not null,
     country varchar(30) not null
 );
@@ -10,8 +10,8 @@ create table if not exists author (
 -- creating book table
 -- this create book table if not exists
 create table if not exists book (
-    id uuid default gen_random_uuid() primary key not null,
-    author_id uuid references author(id) on delete cascade not null,
+    author_id uuid default gen_random_uuid() primary key not null,
+    author_id uuid references author(author_id) on delete cascade not null,
     title varchar(30) not null,
     price decimal not null,
     currency varchar(20) default '$' not null,
