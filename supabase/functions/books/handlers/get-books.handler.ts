9ce1d45c67cd42df203e14a8c4bd4df074ed61b2
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-12-24 18:51:26 +0330

    feat: implementing books route handler

--- supabase/functions/books/handlers/get-books.handler.ts
+++ supabase/functions/books/handlers/get-books.handler.ts
@@ -1,5 +1,26 @@
 import { BooksRequestParamsDTO } from "../dto/books-request.dto.ts";
 import { BooksResponseDTO } from "../dto/books-response.dto.ts";
+import { countBooks, getBook } from "../services/get.book.ts";
 
-export function getBooksHandler(params: BooksRequestParamsDTO): BooksResponseDTO {
+export async function getBooksHandler(
+  params: BooksRequestParamsDTO,
+): Promise<BooksResponseDTO> {
+  const page = Number.parseInt(params.page);
+  const limit = Number.parseInt(params.limit);
+
+  const books = await getBook(
+    params.authorId,
+    params.sort,
+    page,
+    limit,
+  );
+
+  const booksCount = await countBooks();
+
+  return {
+    books: books,
+    page,
+    pageLength: limit,
+    pagesCount: Math.ceil(booksCount / limit),
+  };
 }
